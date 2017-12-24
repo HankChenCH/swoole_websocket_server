03432104e32e8c5d2f182de21e0264adbbac4bbe
HankGHChen <372259464@qq.com>
2017-12-24 12:18:48 +0800

    修改聊天信息广播的逻辑 分开群组聊天和个人聊天的广播方式

--- application/event/manager/Chat.php
+++ application/event/manager/Chat.php
@@ -10,7 +10,7 @@ class Chat extends BaseEvent
     public static function message($from, $data)
     {
     	self::broadcastMyGroup($from, function ($fd, $userInfo) use ($data){
-			if ($data->to_type === '1' && $userInfo['uid'] === $data->to_id) {
+			if ($data->to_type === '1' && $userInfo['uid'] === intval($data->to_id)) {
 	        	self::push($fd, $data);
 			} elseif ($data->to_type === '2') {
 	        	self::push($fd, $data);
